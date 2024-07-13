5b75aeb37e7f23b93f463a19486f4651b7b9226c
Soh Hong Yu <sohhongyu@gmail.com>
2024-07-13 23:23:46 +0800

    test ShadCN Dialog

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -20,6 +20,15 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import { Toaster } from "@/components/ui/sonner";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 type UserType = {
   user_id: string;
@@ -196,6 +205,22 @@ function Room() {
             </PopoverContent>
           </Popover>
         </div>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline">Edit Profile</Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Edit profile</DialogTitle>
+              <DialogDescription>
+                Make changes to your profile here. Click save when you're done.
+              </DialogDescription>
+            </DialogHeader>
+            <DialogFooter>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
         <Toaster richColors />
       </Overlay>
     </>
